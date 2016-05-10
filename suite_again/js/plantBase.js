a648fd5d699fc7ccc198faf9b42f15fa628416a3
Laurie Waxman <laurie.waxman@gmail.com>
2016-05-10 16:33:08 -0400

    ahhhh

--- suite_again/js/plantBase.js
+++ suite_again/js/plantBase.js
@@ -16,28 +16,19 @@ function Plant(x, y, fillColour, strokeColour, brain){
 }
 
 Plant.prototype.draw = function(){
-	// this.waveAngle += 0.01;
 	var xSmall = this.x-10; 
 	var ySmall = this.y; 
-	var xBig = this.x+10; 	
-	// push();
-	// rotate(Math.cos(this.waveAngle));
+	var xBig = this.x+10; 
+
 	for(var h in this.turtle.history){
 		var l = this.turtle.history[h];
-		// push();
-		// translate(l.x1, l.y1);
-		// rotate(180);
-		//if this.type = stem
-		// line(0, 0, l.x1-l.x2, l.y1-l.y2);
+
 		if(l.x1<xSmall) xSmall = l.x1; 
 		if(l.x2<xSmall) xSmall = l.x2; 
-
 		if(l.x1>xBig) xBig = l.x1; 
 		if(l.x2>xBig) xBig = l.x2; 
-
 		if(l.y1<ySmall) ySmall = l.y1;
 		if(l.y2<ySmall) ySmall = l.y2;
-
 		this.w = (xBig - xSmall);
 		this.h = this.y-ySmall;
 
@@ -45,34 +36,75 @@ Plant.prototype.draw = function(){
 		line(l.x1, l.y1, l.x2, l.y2);
 		stroke(this.stroke);
 		line(l.x1, l.y1, l.x2, l.y2);
-
-
-
-		
-		// if(l.x2-width>0){
-		// 	console.log("off screen width");
-		// 	// var x1 = l.x1 - width	
-		// }
-		// line(l.x1, l.y1+Math.cos(this.waveAngle), l.x2, l.y2+Math.cos(this.waveAngle));
-		//else if this.type = flower
-		//else if this.type = leaf
-		// pop();
 	}
-	if(xBig>width){
-		var rX = xBig-width; 
-		this.repeat(rX, this.y);
+	var rX = this.x;
+	var rY = this.y;
+
+	var repCase = 0; 
+	if(xBig>width && ySmall>0){
+		repCase = 1; 
+	}else if(xSmall<0 && ySmall>0){
+		repCase = 2; 
+	}else if(ySmall<0 && xSmall<0){
+		repCase = 3; 
+	}else if(ySmall<0 && xBig>width){
+		repCase = 4; 
+	}else if(ySmall<0){
+		repCase = 5; 
+	}else{
+		repCase = 0; 
 	}
-	stroke("blue");
-	rect(this.x-(this.x-xSmall), this.y-this.h, this.w, this.h);
-	// console.log(this.w);
-	// pop();
+	this.repeat(repCase);
 };
 
-Plant.prototype.repeat = function(x,y){
-	console.log("repeat", x, y);
-	// stroke(this.bgStroke);
-	// line(x-l.x1, l.y1, l.x2, l.y2);
-	// stroke(this.stroke);
-	// line(x-l.x1, l.y1, l.x2, l.y2);
-
+Plant.prototype.repeat = function(c){
+	for(var h in this.turtle.history){
+		var l = this.turtle.history[h];
+		var xx1 = l.x1;
+		var yy1 = l.y1;
+		var xx2 = l.x2;
+		var yy2 = l.y2;
+		switch (c) {
+			case 0:
+				xx1 = l.x1;
+				yy1 = l.y1;
+				xx2 = l.x2;
+				yy2 = l.y2;
+				break;
+			case 1:
+				xx1 = -(width-l.x1);
+				xx2 = -(width-l.x2);
+				break;
+			case 2:
+				xx1 = (x+l.x1);
+				xx2 = (x+l.x2);
+				break;
+			case 3:
+				xx1 = (x+l.x1);
+				xx2 = (x+l.x2);
+				yy1 = height+l.y1;
+				yy2 = height+l.y2;
+				break;
+			case 4:
+				xx1 = -(width-l.x1);
+				xx2 = -(width-l.x2);
+				yy1 = height+l.y1;
+				yy2 = height+l.y2;
+				break;
+			case 5:
+				yy1 = height+l.y1;
+				yy2 = height+l.y2;
+				break;
+			default:
+				xx1 = l.x1;
+				yy1 = l.y1;
+				xx2 = l.x2;
+				yy2 = l.y2;
+				break;
+		}
+		stroke(this.bgStroke);
+		line(xx1, yy1, xx2, yy2);
+		stroke(this.stroke);
+		line(xx1, yy1, xx2, yy2);
+	}
 };
